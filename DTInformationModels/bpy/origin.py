ad0f36a6812a110f011805b6b4e2526283950c28
Ran Wei <r.wei@live.co.uk>
2025-08-28 12:43:40 +0100

    .

--- DTInformationModels/bpy/origin.py
+++
@@ -1,7 +0,0 @@
-import bpy
-
-for object in bpy.context.scene.objects.values():
-    bpy.ops.object.select_all(action='DESELECT')
-
-    object.select_set(True)
-    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
